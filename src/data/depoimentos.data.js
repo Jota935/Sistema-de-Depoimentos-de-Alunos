0b65b3a3cda846a5445d8307966273503df63898
Jota935 <147872543+Jota935@users.noreply.github.com>
2026-05-11 23:28:01 -0100

    Add files via upload

---
+++ src/data/depoimentos.data.js
@@ -0,0 +1,22 @@
+const depoimentos = [
+  {
+    id: 1,
+    nomeAluno: 'Kelly Silva',
+    curso: 'Engenharia Informática',
+    mensagem: 'Gostei muito da disciplina porque aprendi a criar APIs de forma prática.',
+    nota: 5,
+    status: 'aprovado',
+    dataCriacao: '2026-04-20'
+  },
+  {
+    id: 2,
+    nomeAluno: 'João Lima',
+    curso: 'Gestão',
+    mensagem: 'As aulas ajudaram-me a compreender melhor o uso de sistemas web.',
+    nota: 4,
+    status: 'pendente',
+    dataCriacao: '2026-04-20'
+  }
+];
+
+module.exports = depoimentos;
